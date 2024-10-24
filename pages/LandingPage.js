69c3777f703e62a69c81ebbfee0842a1d386ba8a
rpaigemei <paigereeves@ufl.edu>
2024-10-24 16:12:18 -0400

    nav foundation

---
+++ pages/LandingPage.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function LandingPage() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Landing Page</Text>
+        </View>
+    );
+}
+
+export default LandingPage;
