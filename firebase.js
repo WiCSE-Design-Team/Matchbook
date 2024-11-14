1bd7389e863ae0c840c80671bdcda71cab00d28c
rpa0309 <anstettm@ufl.edu>
2024-11-14 15:13:03 -0500

    update firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAggtsPb32oNVGgLmbFKSV53hWLWHylFsQ",
+  authDomain: "matchbook-e8edb.firebaseapp.com",
+  projectId: "matchbook-e8edb",
+  storageBucket: "matchbook-e8edb.firebasestorage.app",
+  messagingSenderId: "706233936861",
+  appId: "1:706233936861:web:e3c9297137eb27c8e4a434"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
