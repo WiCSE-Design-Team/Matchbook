caf555c3daca817ba3b17289b043a85871d6204e
rpaigemei <paigereeves@ufl.edu>
2025-02-03 11:55:22 -0500

    styled bottom nav

--- components/TabNav.js
+++ components/TabNav.js
@@ -1,20 +1,33 @@
-import * as React from 'react';
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 // screens
 import MatchingPage from '../pages/MatchingPage';
 import ProfilePage from '../pages/ProfilePage';
 import ChatPage from '../pages/ChatPage';
-import ProfileCreationPage from '../pages/ProfileCreationPage';
+
+const screens = [
+    {route: 'Profile', label: 'Profile', component: ProfilePage, icon: 'account'},
+    {route: 'Matching', label: 'Matching', component: MatchingPage, icon: 'book'},
+    {route: 'Chat', label: 'Chat', component: ChatPage, icon: 'chat'}
+];
 
 const Tab = createBottomTabNavigator();
 
 function TabNav() {
     return (
-        <Tab.Navigator initialRouteName='Matching'>
-            <Tab.Screen name='Profile' component={ProfilePage} options={{ headerShown: false}} />
-            <Tab.Screen name='Matching' component={MatchingPage} options={{ headerShown: false}} />
-            <Tab.Screen name='Messages' component={ChatPage} options={{ headerShown: false}} />
+        <Tab.Navigator initialRouteName='Matching'  screenOptions={{ headerShown: false, tabBarStyle: { height: 90, paddingBottom: 10 } }}>
+            {screens.map((item, index) => {
+                return (
+                    <Tab.Screen key={index} name={item.route} component={item.component}
+                        options={{
+                            tabBarShowLabel: false,
+                            tabBarIcon: ({ focused }) => (<MaterialCommunityIcons name={item.icon} size={focused ? 40 : 32} color={focused ? '#9E122C' : '#CE8895'} />)
+                        }}
+                    />
+                )
+            })}
         </Tab.Navigator>
     )
 }
