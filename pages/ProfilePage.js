c88446b4b91e79622f2ec4591fd8a21b6b9b23d5
Alexa <227alexa@gmail.com>
2025-02-20 15:04:40 -0500

    finished profile page

--- pages/ProfilePage.js
+++ pages/ProfilePage.js
@@ -1,12 +1,11 @@
-import * as React from 'react';
+import React, { useRef } from 'react';
 import {View, Text, Image, ImageBackground, ScrollView, StyleSheet} from 'react-native';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
-
+import { FontAwesome5 } from '@expo/vector-icons';
 import { profilePage } from '../styling';
 
 function ProfilePage() {
-
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <View style={{
@@ -19,20 +18,12 @@ function ProfilePage() {
                 alignItems: 'center'      
             }} />
 
-                <View style={{
-                width: '90%',   
-                height: 275, 
-                backgroundColor: '#FFFFC5',
-                position: 'absolute',
-                borderRadius: 20, 
-                top: 60,       
-                }} />
-
                 <Text style={{
                 fontSize: 15,
-                fontFamily: 'times new roman',
+                fontFamily: 'Karla-Bold',
                 top: 175, 
-                position: 'absolute'
+                position: 'absolute', 
+                color: 'white'
                 }}>profile picture here</Text>
             
             <View style={{
@@ -41,43 +32,95 @@ function ProfilePage() {
                     bottom: 0, 
                     height: 425, 
                     position: 'absolute', 
-                    borderRadius: 30
+                    borderTopRightRadius: 35,
+                    borderTopLeftRadius: 35
                 }}>
 
             <Text style={{
                 fontSize: 30, 
-                fontFamily: 'times new roman', 
+                fontFamily: 'Bakso Sapi', 
                 color: '#9E122C', 
                 position: 'absolute', 
-                top: 10, 
-                left: 20
+                top: 15, 
+                left: 10
             }}> Name, Age </Text>
 
             <Text style={{
                 fontSize: 18,
-                fontFamily: 'times new roman', 
+                fontFamily: 'Karla-Bold', 
                 color: '#F99D90', 
                 position: 'absolute', 
-                top: 18, 
-                right: 150
+                top: 22, 
+                right: 120
             }}> pronouns </Text>
 
             <SimpleLineIcons name="settings" size={22} color="#9E122C" 
                 style={{
                     position: 'absolute', 
-                    top: 18,
+                    top: 22,
                     right: 20
-                    }} />
+                }} />
+
+            <Text style ={{
+                fontFamily: 'Karla-Bold', 
+                color: '#FBCB77', 
+                fontSize: 15, 
+                position: 'absolute', 
+                top: 65, 
+                left: 20
+            }}>University </Text>
+
+            <Text style ={{
+                fontFamily: 'Karla-Bold', 
+                fontSize: 15, 
+                position: 'absolute', 
+                top: 65, 
+                right: 20
+            }}>University of Florida</Text>
+
+            <Text style ={{
+                fontFamily: 'Karla-Bold', 
+                color: '#FBCB77', 
+                fontSize: 15, 
+                position: 'absolute', 
+                top: 90, 
+                left: 20
+            }}>Academic Year </Text>
+
+            <Text style ={{
+                fontFamily: 'Karla-Bold', 
+                fontSize: 15, 
+                position: 'absolute', 
+                top: 90, 
+                right: 20
+            }}>Freshman</Text>
+
+            <Text style ={{
+                fontFamily: 'Karla-Bold', 
+                color: '#FBCB77', 
+                fontSize: 15, 
+                position: 'absolute', 
+                top: 115, 
+                left: 20
+            }}>Major </Text>
+
+            <Text style ={{
+                fontFamily: 'Karla-Bold', 
+                fontSize: 15, 
+                position: 'absolute', 
+                top: 115, 
+                right: 20
+            }}>Computer Science</Text>
 
             <Text style={{
                 position: 'absolute', 
-                fontFamily: 'times new roman', 
+                fontFamily: 'Karla', 
                 fontSize: 18, 
-                top: 50, 
-                left: 20
+                top: 145, 
+                left: 15
                 }}> Introduction or bio! </Text>
             
-            <View style={[styles.courseContainer, {top: 75}]}>
+            <View style={[styles.courseContainer, {top: 375}]}>
                 <View style={styles.container}>
                     <Text style={styles.text}>course name</Text>
                 </View>
@@ -85,27 +128,28 @@ function ProfilePage() {
                     <Text style={styles.text}>course name</Text>
                 </View>
             </View>
-
-                <ScrollView> 
-                <View style = {{top: 90, width: '100%', paddingLeft: 20}}>
-                    <Text style={{marginBottom: 20, textAlign: "center", width: '100%'}}>More Profile Info</Text>
         
-                    <Text>Education:</Text>
-                        <Text style={{marginLeft: 20}}>School:</Text>
-                        <Text style={{marginLeft: 20}}>Academic Year:</Text>
-                        <Text style={{marginLeft: 20}}>Major:</Text>
-                        <Text style={{marginLeft: 20}}>Minors:</Text>
-                        <Text style={{marginLeft: 20, marginBottom: 20}}>Professors:</Text>
-
-                    <Text>Preferences:</Text>
-                        <Text style={{marginLeft: 20}}>Study Style:</Text>
-                        <Text style={{marginLeft: 20}}>Favorite Library:</Text>
+        <View style={styles.promptContainer}>
+            <ScrollView
+                horizontal = {true}
+                contentContainerStyle={styles.scrollViewContent}
+                showsHorizontalScrollIndicator={true}
+            > 
+                <View style={styles.prompt}>
+                    <Text style={styles.promptText}>Prompt 1: </Text>
                 </View>
-                </ScrollView>
-            </View>
-            
+                <View style={styles.prompt}>
+                    <Text style={styles.promptText}>Prompt 2</Text>
+                </View>
+                <View style={styles.prompt}>
+                    <Text style={styles.promptText}>Prompt 3</Text>
+                </View>
+            </ScrollView>
         </View>
-    );
+    </View>
+            
+</View>
+);
 }
 
 const styles = StyleSheet.create({
@@ -117,15 +161,52 @@ const styles = StyleSheet.create({
       height: 30,
       borderRadius: 20,
       borderWidth: 2,
-      borderColor: 'black',
+      borderColor: '#9E122C',
       justifyContent: 'center',
       alignItems: 'center',
-      marginLeft: 20,  
+      marginLeft: 15,  
     },
+
     text: {
       fontSize: 12,
-      color: 'black',
+      fontFamily: 'Karla', 
+      color: '#9E122C',
     },
+
+    promptContainer: {
+       width: '100%', 
+       height: 150, 
+       top: 170, 
+       flex: 1, 
+       justifyContent: 'center', 
+       alignItems: 'center', 
+    }, 
+
+    prompt: {
+      width: 355, 
+      height: 150,
+      borderRadius: 20,
+      borderWidth: 2,
+      borderColor: '#FBCB77',
+      alignItems: 'center',
+      marginLeft: 20, 
+    }, 
+
+    promptText:  {
+        fontFamily: 'Bakso Sapi', 
+        fontSize: 20, 
+        color: '#FBCB77'
+    }, 
+
+    scrollViewContent: {
+        flexDirection: 'row'
+    }, 
+
+    scrollView: {
+        width: '100%', 
+        flex: 1,
+        flexDirection: 'row'
+    }
   });
 
 export default ProfilePage;
