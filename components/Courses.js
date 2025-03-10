fba21ab96fd9a148385eae1761df4a874752db1f
Alexa <227alexa@gmail.com>
2025-03-10 14:19:42 -0400

    courses added

---
+++ components/Courses.js
@@ -0,0 +1,357 @@
+const CISEcourses = [
+    {
+        courseCode: 'CAI 4104',
+        courseName: 'Machine Learning Engineering',
+    
+    },
+    {
+        courseCode: 'CAP 3020',
+        courseName: 'Theory and Practice of Multimedia Production',
+    
+    },
+    {
+        courseCode: 'CAP 3027',
+        courseName: 'Introduction to Computational Media',
+    
+    },
+    {
+        courseCode: 'CAP 3032',
+        courseName: 'Interactive Modeling and Animation 1',
+    
+    },
+    {
+        courseCode: 'CAP 3034',
+        courseName: 'Introduction to Computer-Aided Animation',
+    
+    },
+    {
+        courseCode: 'CAP 3220',
+        courseName: 'Introduction to Computer-Aided Modeling',
+    
+    },
+    {
+        courseCode: 'CAP 4053',
+        courseName: 'Artificial Intelligence for Computer Games',
+    
+    },
+    {
+        courseCode: 'CAP 4112',
+        courseName: 'Generating Expressiveness in Intelligent Agents and Avatars',
+    
+    },
+    {
+        courseCode: 'CAP 4136',
+        courseName: 'Malware Reverse Engineering',
+    
+    },
+    {
+        courseCode: 'CAP 4410',
+        courseName: 'Computer Vision',
+    
+    },
+    {
+        courseCode: 'CAP 4613',
+        courseName: 'Deep Learning for Computer Graphics',
+    
+    },
+    {
+        courseCode: 'CAP 4621',
+        courseName: 'Artificial Intelligence and Heuristics',
+    
+    },
+    {
+        courseCode: 'CAP 4641',
+        courseName: 'Natural Language Processing',
+    
+    },
+    {
+        courseCode: 'CAP 4730',
+        courseName: 'Computational Structures in Computer Graphics',
+    
+    },
+    {
+        courseCode: 'CAP 4770',
+        courseName: 'Introduction to Data Science',
+    
+    },
+    {
+        courseCode: 'CAP 4773',
+        courseName: 'Projects Data Science',
+    
+    },
+    {
+        courseCode: 'CDA 3101',
+        courseName: 'Introduction to Computer Organization',
+    
+    },
+    {
+        courseCode: 'CDA 4102',
+        courseName: 'Computer Architecture',
+    
+    },
+    {
+        courseCode: 'CDA 4324C',
+        courseName: 'Cyber-physical System Security',
+    
+    },
+    {
+        courseCode: 'CDA 4630',
+        courseName: 'Embedded Systems',
+    
+    },
+    {
+        courseCode: 'CEN 3031',
+        courseName: 'Introduction to Software Engineering',
+    
+    },
+    {
+        courseCode: 'CEN 3907C',
+        courseName: 'Computer Engineering Design 1',
+    
+    },
+    {
+        courseCode: 'CEN 3913',
+        courseName: 'Computer and Information Science and Engineering Design 1',
+    
+    },
+    {
+        courseCode: 'CEN 4072',
+        courseName: 'Software Testing and Verification',
+    
+    },
+    {
+        courseCode: 'CEN 4721',
+        courseName: 'Human-Computer Interaction',
+    
+    },
+    {
+        courseCode: 'CEN 4722',
+        courseName: 'User Experience Design',
+    
+    }, 
+    {
+        courseCode: 'CEN 4725',
+        courseName: 'Natural User Interaction',
+    
+    },
+    {
+        courseCode: 'CEN 4730',
+        courseName: 'Human-Centered Input Recognition Algorithms',
+    
+    },
+    {
+        courseCode: 'CEN 4908C',
+        courseName: 'Computer Engineering Design 2',
+    
+    },
+    {
+        courseCode: 'CEN 4914',
+        courseName: 'Computer and Information Science and Engineering Design 2',
+    
+    },
+    {
+        courseCode: 'CGS 2032',
+        courseName: 'Math, Art and Computing',
+    
+    },
+    {
+        courseCode: 'CGS 3063',
+        courseName: 'Computers and Modern Society',
+    
+    },
+    {
+        courseCode: 'CGS 3065',
+        courseName: 'Legal and Social Issues in Computing',
+    
+    },
+    {
+        courseCode: 'CGS 4144',
+        courseName: 'Introduction to Bioinformatic Algorithms',
+    
+    },
+    {
+        courseCode: 'CGS 4853C',
+        courseName: 'Introduction to Web Development and Design',
+    
+    },
+    {
+        courseCode: 'CIS 4204',
+        courseName: 'Penetration Testing: Ethical Hacking',
+    
+    },
+    {
+        courseCode: 'CIS 4213',
+        courseName: 'Enterprise Security',
+    
+    },
+    {
+        courseCode: 'CIS 4301',
+        courseName: 'Information and Database Systems 1',
+    
+    },
+    {
+        courseCode: 'CIS 4360',
+        courseName: 'Computer and Information Security',
+    
+    }, 
+    {
+        courseCode: 'CIS 4362',
+        courseName: 'Introduction to Cryptology',
+    
+    },
+    {
+        courseCode: 'CIS 4715',
+        courseName: 'CS Teaching & Learning',
+    
+    },
+    {
+        courseCode: 'CIS 4905',
+        courseName: 'Individual Study in CISE',
+    
+    },
+    {
+        courseCode: 'CIS 4912C',
+        courseName: 'Integrated Product and Process Design 1',
+    
+    },
+    {
+        courseCode: 'CIS 4913C',
+        courseName: 'Integrated Product and Process Design 2',
+    
+    },
+    {
+        courseCode: 'CIS 4914',
+        courseName: 'Senior Project',
+    
+    },
+    {
+        courseCode: 'CIS 4930',
+        courseName: 'Special Topics in CISE',
+    
+    },
+    {
+        courseCode: 'CIS 4940',
+        courseName: 'Practical Work',
+    
+    },
+    {
+        courseCode: 'CIS 4949',
+        courseName: 'Co-Op Work in CISE',
+    
+    },
+    {
+        courseCode: 'CIS 4956',
+        courseName: 'Overseas Studies',
+    
+    },
+    {
+        courseCode: 'CNT 4007',
+        courseName: 'Computer Network Fundamentals',
+    
+    },
+    {
+        courseCode: 'CNT 4520',
+        courseName: 'Introduction to Mobile Networking',
+    
+    },
+    {
+        courseCode: 'CNT 4731',
+        courseName: 'Multimedia Networking Principles',
+    
+    },
+    {
+        courseCode: 'COP 2271',
+        courseName: 'Computer Programming for Engineers',
+    
+    },
+    {
+        courseCode: 'COP 2800',
+        courseName: 'Computer Programming Using JAVA',
+    
+    }, 
+    {
+        courseCode: 'COP 3013',
+        courseName: 'Programming Language Survey',
+    
+    },
+    {
+        courseCode: 'COP 3275',
+        courseName: 'Computer Programming Using C',
+    
+    },
+    {
+        courseCode: 'COP 3502C',
+        courseName: 'Programming Fundamentals 1',
+    
+    },
+    {
+        courseCode: 'COP 3503C',
+        courseName: 'Programming Fundamentals 2',
+    
+    },
+    {
+        courseCode: 'COP 3504C',
+        courseName: 'Advanced Programming Fundamentals for CIS Majors',
+    
+    },
+    {
+        courseCode: 'COP 3530',
+        courseName: 'Data Structures and Algorithms',
+    
+    },
+    {
+        courseCode: 'COP 4020',
+        courseName: 'Programming Language Concepts',
+    
+    },
+    {
+        courseCode: 'COP 4331',
+        courseName: 'Object-oriented Programming',
+    
+    },
+    {
+        courseCode: 'COP 4533',
+        courseName: 'Algorithm Abstraction and Design',
+    
+    },
+    {
+        courseCode: 'COP 4554',
+        courseName: 'Programming Language Inquiry',
+    
+    },
+    {
+        courseCode: 'COP 4600',
+        courseName: 'Operating Systems',
+    
+    },
+    {
+        courseCode: 'COP 4620',
+        courseName: 'Translators and Translator Writing Systems',
+    
+    },
+    {
+        courseCode: 'COP 4720',
+        courseName: 'Information and Database Systems 2',
+    
+    },
+    {
+        courseCode: 'COT 3100',
+        courseName: 'Applications of Discrete Structures',
+    
+    },
+    {
+        courseCode: 'COT 4501',
+        courseName: 'Numerical Analysis: A Computational Approach',
+    
+    },
+    {
+        courseCode: 'EGN 4912',
+        courseName: 'Engineering Directed Independent Research',
+    
+    },
+    {
+        courseCode: 'IDC 4710',
+        courseName: 'Virtual Reality for the Social Good',
+    
+    }
+]
